99afb38d3f86d7a4610b8e7f080bb65809d0d350
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-04-03 00:15:43 +0200

    logic in urls

--- twitter/app/urls.py
+++ twitter/app/urls.py
@@ -3,5 +3,7 @@ from app import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("users/<str:username>/",
+         views.UserDetailView.as_view(), name="user_detail"),
 
 ]
