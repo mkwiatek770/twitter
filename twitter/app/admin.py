c1d98ee888f6ad67ac3b4f30d90410050dae944b
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-04-03 00:14:57 +0200

    Added models ProfileUser, Tweet, TweetComment, TweetLike and registered them

--- twitter/app/admin.py
+++ twitter/app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import Tweet, TweetLike, TweetShare, ProfileUser
 # Register your models here.
+
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TweetLike)
+class TweetLikeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TweetShare)
+class TweetShareAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProfileUser)
+class ProfileUserAdmin(admin.ModelAdmin):
+    pass
--- twitter/app/models.py
+++ twitter/app/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
+# naprawić funkcjonalność żeby użyktownik musiał podać mail
+
 
 class ProfileUser(AbstractUser):
     email = models.EmailField(null=False)
     tweets = models.IntegerField(default=0)
     followers = models.ManyToManyField("self")
+    avatar = models.ImageField(default="img/man.png")
 
     def __str__(self):
         return self.username
@@ -14,9 +17,11 @@ class ProfileUser(AbstractUser):
 
 class Tweet(models.Model):
     author = models.ForeignKey(ProfileUser, on_delete=models.CASCADE)
-    content = models.CharField(max_length=300)
+    content = models.TextField(max_length=300)
     creation_date = models.DateTimeField(auto_now_add=True)
     last_modification = models.DateTimeField()
+    likes = models.IntegerField(default=0)
+    comments = models.ManyToManyField('TweetComment')
 
     def save(self, *args, **kwargs):
         self.last_modification = timezone.now()
@@ -24,6 +29,15 @@ class Tweet(models.Model):
 
     def __str__(self):
         return "{} - {} ...".format(self.author, self.content[:20])
+
+
+class TweetComment(models.Model):
+    user = models.ForeignKey(ProfileUser, on_delete=models.CASCADE)
+    content = models.TextField(max_length=120)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} - {} ...".format(self.user, self.content[:20])
 
 
 class TweetLike(models.Model):
