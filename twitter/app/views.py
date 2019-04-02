8cc05a2a20a63f15094b20c4b0800a534f26317d
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-04-03 00:18:50 +0200

    HomeView, TweetCreateView

--- twitter/app/views.py
+++ twitter/app/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
+from django.views.generic import (
+    ListView,
+    CreateView,
+
+)
+from .models import Tweet, ProfileUser
+from .forms import TweetCreateForm
 
 
-class HomeView(View):
+class HomeView(ListView):
+    model = Tweet
+    context_object_name = 'tweets'
+    template_name = 'app/home.html'
+
+
+class TweetCreateView(View):
     def get(self, request):
-        return render(request, "app/home.html")
+        form = TweetCreateForm()
+        return render(request, "app/create_tweet.html", {
+            'form': form
+        })
 
     def post(self, request):
-        pass
+        form = TweetCreateForm(request.POST)
+
+        if form.is_valid():
+            author = request.user
+            content = form.cleaned_data['content']
+
+            Tweet.objects.create(
+                author=author,
+                content=content
+            )
+
+            return redirect(reverse("home"))
+
+
+class UserDetailView(View):
+    def get(self, request, username):
+        user = ProfileUser.objects.get(username=username)
